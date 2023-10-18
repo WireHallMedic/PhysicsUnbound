f36a649f273a72b7a1659b7e29e31d8f4646fec0
Michael <MichaelWidler@yahoo.com>
2023-10-18 11:25:39 -0700

    Create enum for tile types.

---
+++ PhysicsUnlocked/GeometryType.java
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+package PhysicsUnlocked;
+
+public enum GeometryType
+{
+   EMPTY,
+   FULL,
+   ASCENDING_FLOOR,
+   DESCENDING_FLOOR,
+   ASCENDING_CEILING,
+   DESCENDING_CEILING;
+}
