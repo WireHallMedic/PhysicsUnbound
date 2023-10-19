f382ab817de6ff781b4893ef8a053be7442850d3
Michael <MichaelWidler@yahoo.com>
2023-10-19 07:44:54 -0700

    Create test file.

--- PhysicsUnlocked/Line.java
+++ PhysicsUnlocked/Line.java
@@ -1,5 +1,5 @@
 /*
-An interface for moving objects to handle collisions with other objects and geometry as an event
+A class for mathematical lines. Kept in the form of y = mx + b. We hold  m, b, and a point on the line
 */
 
 package PhysicsUnlocked;
@@ -7,5 +7,31 @@ package PhysicsUnlocked;
 
 public class Line
 {
+	private double m;
+	private double b;
+	private DoublePair point;
 
+
+	public double getM(){return m;}
+	public double getB(){return b;}
+	public DoublePair getPoint(){return new DoublePair(point);}
+
+
+	public void setM(double m){m = m;}
+	public void setB(double b){b = b;}
+	public void setPoint(DoublePair p){point = p;}
+
+
+   public Line(DoublePair origin, DoublePair slope)
+   {
+      point = new DoublePair(origin);
+      m = slope.y / slope.x;
+      b = origin.y - (m * origin.x);
+   }
+   
+   @Override
+   public String toString()
+   {
+      return String.format("y = %f.3x + %f.3", m, b);
+   }
 }
