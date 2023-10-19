ca1991ee12a4854f5b484597f80d18301e8f6aa2
Michael <MichaelWidler@yahoo.com>
2023-10-19 08:27:57 -0700

    Implement determining intersection of lines.

--- PhysicsUnlocked/Line.java
+++ PhysicsUnlocked/Line.java
@@ -13,13 +13,10 @@ public class Line
 
 
 	public double getM(){return m;}
+   public double getSlope(){return m;}
 	public double getB(){return b;}
+   public double getIntercept(){return b;}
 	public DoublePair getPoint(){return new DoublePair(point);}
-
-
-	public void setM(double m){m = m;}
-	public void setB(double b){b = b;}
-	public void setPoint(DoublePair p){point = p;}
 
 
    public Line(DoublePair origin, DoublePair slope)
@@ -32,6 +29,27 @@ public class Line
    @Override
    public String toString()
    {
-      return String.format("y = %f.3x + %f.3", m, b);
+      return String.format("y = %.3fx + %.3f", m, b);
+   }
+   
+   public boolean hasIntersection(Line that)
+   {
+      return this.m != that.m;
+   }
+   
+   public DoublePair getIntersection(Line that)
+   {
+      double[] a = this.getStandardForm();
+      double[] b = that.getStandardForm();
+      double d  = (a[0] * b[1]) - (a[1] * b[0]);
+      double dx = (a[2] * b[1]) - (a[1] * b[2]);
+      double dy = (a[0] * b[2]) - (a[2] * b[0]);
+      return new DoublePair(dx / d, dy / d);
+   }
+   
+   public double[] getStandardForm()
+   {
+      double[] answer = {m, -1.0, -b};
+      return answer;
    }
 }
--- PhysicsUnlocked/LineTest.java
+++ PhysicsUnlocked/LineTest.java
@@ -19,7 +19,27 @@ public class LineTest
    /** A test that always fails. **/
    @Test public void defaultTest() 
    {
-      Assert.assertEquals("Default test added by jGRASP. Delete "
-            + "this test once you have added your own.", 0, 1);
+      DoublePair origin = new DoublePair(0.0, 1.0);
+      DoublePair slope = new DoublePair(-1.0, 1.0);
+      Line lineA = new Line(origin, slope);
+      Assert.assertEquals("Line A has expected slope", -1.0, lineA.getSlope(), .001);
+      Assert.assertEquals("Line A has expected intercept", 1.0, lineA.getIntercept(), .001);
+      
+      origin = new DoublePair(0.0, -1.0);
+      slope = new DoublePair(1.0, 1.0);
+      Line lineB = new Line(origin, slope);
+      Assert.assertEquals("Line B has expected slope", 1.0, lineB.getSlope(), .001);
+      Assert.assertEquals("Line B has expected intercept", -1.0, lineB.getIntercept(), .001);
+      
+      DoublePair expectedIntersection = new DoublePair(1.0, 0.0);
+      Assert.assertTrue("Lines A and B intersect", lineA.hasIntersection(lineB));
+      Assert.assertTrue("Lines A and B have expected intersection", expectedIntersection.equals(lineA.getIntersection(lineB)));
+      Assert.assertTrue("Reverse returns same result", expectedIntersection.equals(lineB.getIntersection(lineA)));
+      
+      Assert.assertFalse("Colinear lines do not have intersection", lineA.hasIntersection(lineA));
+      origin.x += 3.4;
+      Line lineC = new Line(origin, slope);
+      Assert.assertFalse("Parallel lines do not have intersection", lineB.hasIntersection(lineC));
+      
    }
 }
